082b725f94389d8a10a234bd95f80ed5dd138fca
Sergii Puzyrov <sergii.puzyrov@gmail.com>
2018-01-24 10:28:13 +0200

    Added sample of ICMP ping with WinSock2

---
+++ PingDemo/stdafx.h
@@ -0,0 +1,21 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <winsock2.h>
+#include <iphlpapi.h>
+#include <icmpapi.h>
+#include <stdio.h>
+
+#pragma comment(lib, "iphlpapi.lib")
+#pragma comment(lib, "ws2_32.lib")
+
+
+
+
+// TODO: reference additional headers your program requires here

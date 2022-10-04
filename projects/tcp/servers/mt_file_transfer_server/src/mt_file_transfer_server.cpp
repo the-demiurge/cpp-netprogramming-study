dc55b294219f7201048d1e3fcfb2d60a09659ef9
svpuzyrov <svpuzyrov@gmail.com>
2022-10-04 13:12:18 +0300

    CHECK_IO instead of CHECK_VOID_IO, add missing response

--- projects/tcp/servers/mt_file_transfer_server/src/mt_file_transfer_server.cpp
+++ projects/tcp/servers/mt_file_transfer_server/src/mt_file_transfer_server.cpp
@@ -13,18 +13,21 @@ THREAD_RESULT handle_connection(void *data) {
 
     auto rc = recv(socket, (char *) &file_header, sizeof(file_header), 0);
 
-    CHECK_VOID_IO((rc > 0), "Can't send received file header")
+    //CHECK_VOID_IO((rc > 0), "Can't send received file header")
+    CHECK_IO(!(rc > 0), -1, "Can't send received file header")
 
     struct FileTransferResult result;
     memset(&file_header, 0, sizeof(file_header));
     isOK(&file_header, &result);
 
     rc = send(socket, (char *) &result, sizeof(result), 0);
-    CHECK_VOID_IO((rc > 0), "Can't send result to client")
+    //CHECK_VOID_IO((rc > 0), "Can't send result to client")
+    CHECK_IO(!(rc > 0), -1, "Can't send result to client")
 
     if (FileTransferResult::ACCEPTED != result.status) {
         printf("Can't send %s file\n", file_header.name);
-        return VOID_DEFAULT_RESULT;
+        //return VOID_DEFAULT_RESULT;
+        return -1;
     }
 
     struct FileContent file_content;
@@ -40,6 +43,8 @@ THREAD_RESULT handle_connection(void *data) {
         if (!file_content.is_closed) {
             file.write(file_content.buffer, file_content.count);
         }
+        result.status = FileTransferResult::OK;//My
+        ret = send(socket, (char*)&result, sizeof(result), 0);//My
     }
 
     file.close();
